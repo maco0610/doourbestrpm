afe101e3bbdc0223a53bbdd1bf415512c05598bb
Kwon Hye-ryung <leja0418@gmail.com>
2020-01-07 00:42:47 +0900

    test
    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        //dev/search
-        //search/hyeryung
-
+        //dev/searchsss
     }
-    //
+
 }
